06001ea423f6a57223f0b1fb2babbedff67c0c32
Nozim Yusupov <nozimy@yyandex.ru>
2022-02-16 05:18:37 +0500

    add cart get numbers of items

--- public/app.js
+++ public/app.js
@@ -12,3 +12,15 @@ function addToCart(id) {
   window.localStorage.setItem(key, x);
 }
 
+function cartGetNumberOfItems() {
+  var cnt = 0;
+  for (var i = 0; i < window.localStorage.length; i++) {
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+
+    if (key.indexOf('product_') == 0) {
+      cnt = cnt + value*1;
+    }
+  }
+  return cnt;
+}
